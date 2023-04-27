c8cc63b759342b11479ccdc416ed374d69d7e9fb
Stan1i <alexandersamedov@gmail.com>
2023-04-27 22:49:11 +0300

    first commit

--- loreal.js
+++ loreal.js
@@ -20,7 +20,7 @@ app.use(
     cors({
     })
 );
-app.use(express.json({ limit: "50mb" }))
+app.use(express.json({ limit: "70mb" }))
 
 app.use('/api/task',TaskRouter )
 app.use('/api/user',UserRouter )
